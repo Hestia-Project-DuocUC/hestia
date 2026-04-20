dbc725db3ce1d63d25a3e1bda35a15db100930cd
Luis Gutiérrez <strakkto2534@gmail.com>
2026-04-20 02:41:03 -0400

    feat: cambio de password de texto plano a hashearla utilizando bcrypt

--- backend/app/routes/usuarios.py
+++ backend/app/routes/usuarios.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models.usuario import Usuario
 from app.schemas.usuario import UsuarioCreate, UsuarioResponse
+from app.utils.security import hashear_password
 
 router = APIRouter(prefix="/usuarios", tags=["Usuarios"])
 
@@ -15,12 +16,11 @@ def crear_usuario(usuario: UsuarioCreate, db: Session = Depends(get_db)):
     existente = db.query(Usuario).filter(Usuario.email == usuario.email).first()
     if existente:
         raise HTTPException(status_code=400, detail="El email ya está registrado")
-    # Por ahora guardamos el password directamente
-    # En el paso de autenticación esto cambia por un hash real
+    # Por seguridad, no almacenamos la contraseña en texto plano y hashamos la contraseña antes de guardarla
     nuevo = Usuario(
         nombre=usuario.nombre,
         email=usuario.email,
-        password_hash=usuario.password,
+        password_hash=hashear_password(usuario.password),
         rol=usuario.rol
     )
     db.add(nuevo)
